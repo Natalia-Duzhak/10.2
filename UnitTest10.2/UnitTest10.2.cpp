f6967c2ec89f7d5e5d13f28e36862a549a965b13
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2020-12-14 00:49:29 +0300

    Add files via upload

---
+++ UnitTest10.2/UnitTest10.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project10.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest102
+{
+	TEST_CLASS(UnitTest102)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[] = "t.txt";
+			int m = ProcessTXT(fname);
+			Assert::AreEqual(m, 0);
+		}
+	};
+}
